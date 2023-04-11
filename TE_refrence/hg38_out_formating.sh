27b84f88dc3c23eb1442e4837daff8ffbae398a0
imcalledlewis <103501240+imcalledlewis@users.noreply.github.com>
2023-04-11 10:22:02 +0100

    Update hg38_out_formating.sh
    altered the script to now remove headers and produce a .bed file
    some sections were commented out as they were not needed for example the inclusion of the headers

--- TE_refrence/hg38_out_formating.sh
+++ TE_refrence/hg38_out_formating.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #$ -pe smp 6
 #$ -l h_vmem=4G
+#!/bin/bash
+#$ -pe smp 6
+#$ -l h_vmem=4G
 #$ -l h_rt=24:0:0
 #$ -cwd
 #$ -j y
@@ -9,19 +12,19 @@
 #$ -m bea
 
 # Set the file path and column indices to select
-#file="/data/scratch/bt22880/2023-21-03-colorectal-rrbs/2023-06-04-TE_refrence/data/hg38.fa.out"
+file="/data/scratch/bt22880/2023-21-03-colorectal-rrbs/2023-06-04-TE_refrence/data/hg38.fa.out"
 # Set the header names for the selected columns
-headers="Chromosome,Start,End,Name"
+#headers="Chromosome\tStart\tEnd\tRE_name"
 
-#tr -s ' ' ',' < $file > hg38.fa.c.out
-
-
-# Use awk to select the specified columns and output to a new file, skipping the first line
-awk -F',' 'NR>1{print $6,$7,$8,$12}' hg38.fa.c.out > hg38_column.tsv
+# Use awk to select the specified columns and output to a new file, skipping the first two lines
+awk 'NR>3{print $5 "\t" $6 "\t" $7 "\t" $11}' $file > hg38_column.tsv
 
 # Remove "chr" from the Chromosome column and output to a new file
-awk -F'\t' '{sub(/^chr/, "", $1); print}' hg38_column.tsv > hg38_column_clean.tsv
+awk -F'\t' '{sub(/^chr/, "", $1); print}' hg38_column.tsv > hg38.out.bed
 
-# Replace the headers and remove the second row
-sed -i '1s/^/Chromosome,Start,End,RE_name\n/' hg38_column_clean.tsv
-sed -i '2d' hg38_column_clean.tsv
+# Replace the headers and remove empty rows
+#sed -i '1s/^/'"$headers"'\n/' hg38_column_clean.tsv
+#sed -i '/^[[:space:]]*$/d' hg38_column_clean.tsv
+
+
+
