178510ce6a5576b2ca933871ae570fd6b0a4eecb
imcalledlewis <103501240+imcalledlewis@users.noreply.github.com>
2023-05-16 10:57:43 +0100

    Create CalcMethDiff.r
    R script using MethylKit and doParallel to Find differentially methylated bases using multiple cores.

---
+++ Methylkit/CalcMethDiff.r
@@ -0,0 +1,18 @@
+library(methylKit)
+library(doParallel)
+
+ncores = 4
+registerDoParallel(core = ncores)
+
+#load Unite object 
+# Save the meht object as an RDS file
+meth <- readRDS("meth.rds")
+
+
+# Perform differential methylation analysis between the treatment groups specified in the 'rrbsobj' object
+# The default method for differential methylation analysis is a chi-squared test (the 'test' argument can be used specify a test)
+# The results of the differential methylation analysis are stored in the 'myDiff' object
+myDiff <- calculateDiffMeth(meth, mc.cores = ncores, overdispersion = "MN", effect = "wmean", test = "Chisq", slim = FALSE)
+
+#read the myDiff object as an RDS file
+saveRDS(myDiff, file = "myDiff.rds")
